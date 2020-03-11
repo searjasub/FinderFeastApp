b51b9bfe39eda3e1d28a6f86ddd2780d5eccec2e
Beck <bes6896@yahoo.com>
2020-03-10 19:02:21 -0600

    Distance

--- app/src/main/java/lopez/s/finderfeast/ui/nearme/NearMeFragment.java
+++ app/src/main/java/lopez/s/finderfeast/ui/nearme/NearMeFragment.java
@@ -111,7 +111,7 @@ public class NearMeFragment extends Fragment {
 
                             double resLat = response.getJSONArray("restaurants").getJSONObject(i).getJSONObject("restaurant").getJSONObject("location").getDouble("latitude");
                             double resLon = response.getJSONArray("restaurants").getJSONObject(i).getJSONObject("restaurant").getJSONObject("location").getDouble("longitude");
-                            final double distance = getDistance("K", 0, 0, resLon, resLat);
+                            double distance = getDistance("K", 0, 0, resLon, resLat);
                             System.out.println(response.getJSONArray("restaurants").getJSONObject(i).getJSONObject("restaurant").getString("name"));
                             System.out.println(response.getJSONArray("restaurants").getJSONObject(i).getJSONObject("restaurant").getString("featured_image"));
                             System.out.println(distance);
@@ -173,6 +173,8 @@ public class NearMeFragment extends Fragment {
                             cardView.addView(childLinear);
                             linearLayout.addView(cardView);
 
+                            final double distance2 = distance;
+
                             final int finalI = i;
                             cardView.setOnClickListener(new View.OnClickListener() {
                                 @Override
@@ -182,7 +184,7 @@ public class NearMeFragment extends Fragment {
                                         intent.putExtra("name", response.getJSONArray("restaurants").getJSONObject(finalI).getJSONObject("restaurant").getString("name"));
                                         intent.putExtra("url", response.getJSONArray("restaurants").getJSONObject(finalI).getJSONObject("restaurant").getString("url"));
                                         intent.putExtra("rating", response.getJSONArray("restaurants").getJSONObject(finalI).getJSONObject("restaurant").getJSONObject("user_rating").getString("aggregate_rating"));
-                                        intent.putExtra("distance", distance);
+                                        intent.putExtra("distance", distance2 + "");
                                         intent.putExtra("address",response.getJSONArray("restaurants").getJSONObject(finalI).getJSONObject("restaurant").getJSONObject("location").getString("address"));
                                         intent.putExtra("picture", response.getJSONArray("restaurants").getJSONObject(finalI).getJSONObject("restaurant").getString("featured_image"));
                                     } catch (JSONException e) {
